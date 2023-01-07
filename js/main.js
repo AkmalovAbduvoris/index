bf03288b75d03631a7647193c6d5445068f8c772
AkmalovAbduvoris <akmalovabduvoris@gmail.com>
2023-01-07 14:44:08 +0500

    first

---
+++ js/main.js
@@ -0,0 +1,50 @@
+var NAME = prompt('Alisher Navoiy 1441-yilda yugilgammi') ;
+if (NAME == 'ha') {
+    console.log('tori')}
+else{(NAME == 'yoq')
+    console.log('notori')}
+var NAME1 = prompt('pubg mobile qachon yaratilgan') ;
+if (NAME1 == '2018-yil') {
+    console.log('tori')}
+else{ (NAME1 == '') 
+    console.log('notori')}
+var NAME2 = prompt('dunyodagi eng kassabop film') ;
+if (NAME2 == 'avatar') {
+    console.log('tori')}
+else{ (NAME2 == '') 
+    console.log('notori')}
+var NAME3 = prompt('Ranaldo or Messi') ;
+if (NAME3 == 'Ranaldo') {
+    console.log('https://uz.wikipedia.org/wiki/Cristiano_Ronaldo')}
+else{ (NAME3 == 'Messi') 
+    console.log('https://uz.wikipedia.org/wiki/Lionel_Messi')}
+var NAME4 = prompt('Uzbekistan qachon mustaqillikka erishgan') ;
+if (NAME4 == '1991-yil') {
+    console.log('tori')}
+else{ (NAME == 'yoq') 
+    console.log('')}
+var NAME5 = prompt('ingliz tilida nechta zamon bor') ;
+if (NAME5 == '16') {
+    console.log('tori')}
+else{ (NAME5 == '') 
+    console.log('notori')}
+// var NAME6 = prompt('') ;
+// if (NAME6 == 'ha') {
+//     console.log('tori')}
+// else{ (NAME6 == 'yoq') 
+//     console.log('notori')}k
+// var NAME7 = prompt('') ;
+// if (NAME7 == 'ha') {
+//     console.log('tori')}
+// else{ (NAME7 == 'yoq') 
+//     console.log('notori')}
+// var NAME8 = prompt('') ;
+// if (NAME8 == 'ha') {
+//     console.log('tori')}
+// else{ (NAME8 == 'yoq') 
+//     console.log('notori')}
+// var NAME9 = prompt('') ;
+// if (NAME9 == 'ha') {
+//     console.log('tori')}
+// else{ (NAME9 == 'yoq') 
+//     console.log('notori')}
